0f31592c1f544a2504a9705d0aba094ae03f4f31
Dmytro <tschernomord@gmail.com>
2020-09-07 12:03:06 +0200

    Updates

---
+++ precache-manifest.d24a3d7c97482aefd007fd2f116d3126.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a753bae7440a85a1de291e07d7fddfe",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "732c409689d56031a7a3",
+    "url": "/goit-react-hw-02-feedback/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "732c409689d56031a7a3",
+    "url": "/goit-react-hw-02-feedback/static/js/2.c4bb4ced.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.c4bb4ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0214c60e0a5b3fc016b",
+    "url": "/goit-react-hw-02-feedback/static/js/main.5c1f8283.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
